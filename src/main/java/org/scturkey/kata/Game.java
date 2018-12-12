620b320d20c55ecc6f51c4916aee5494b2267b52
Lemi Orhan Ergin <lemi@scotty.com.tr>
2018-12-12 21:12:42 +0300

    adds tests for frame score calculation when frame has strike

--- src/main/java/org/scturkey/kata/Game.java
+++ src/main/java/org/scturkey/kata/Game.java
@@ -6,13 +6,18 @@ public class Game {
     private int rollCounter = 0;
 
     public void roll(int knockedDownPins) {
+        if (knockedDownPins == 10) {
+            rollCounter++;
+        }
         this.knockedDownPins[rollCounter++] = knockedDownPins;
     }
 
     public int score() {
         int totalScore = 0;
         for (int i = 0; i < knockedDownPins.length; i++) {
-            if (i > 0 && i % 2 == 1) {
+            if (knockedDownPins[i] == 10) {
+                totalScore += knockedDownPins[i + 1] + knockedDownPins[i + 2];
+            } else if (i > 0 && i % 2 == 1) {
                 int frameScore = knockedDownPins[i] + knockedDownPins[i - 1];
                 if (frameScore == 10) {
                     totalScore += knockedDownPins[i + 1];
--- src/test/java/org/scturkey/kata/GameTest.java
+++ src/test/java/org/scturkey/kata/GameTest.java
@@ -46,6 +46,69 @@ public class GameTest {
         assertThat(game.score()).isEqualTo(18);
     }
 
+    @Test
+    void should_calculateFinalScore_when_spareAfterSpare() {
+        Game game = createNewGame();
+        game.roll(8);
+        game.roll(2);
+        game.roll(7);
+        game.roll(3);
+        game.roll(5);
+        assertThat(game.score()).isEqualTo(37);
+    }
+
+    @Test
+    void should_calculateFinalScore_when_itsTimeForStrike() {
+        Game game = createNewGame();
+        game.roll(10);
+        game.roll(2);
+        game.roll(7);
+        game.roll(3);
+        game.roll(5);
+        assertThat(game.score()).isEqualTo(36);
+    }
+
+    @Test
+    void should_calculateFinalScore_when_itsTimeForStrike2() {
+        Game game = createNewGame();
+        game.roll(2);
+        game.roll(7);
+        game.roll(10);
+        game.roll(3);
+        game.roll(5);
+        assertThat(game.score()).isEqualTo(35);
+    }
+
+    @Test
+    void should_calculateScoreOfSecondFrame_when_itsTimeForStrike() {
+        Game game = createNewGame();
+        game.roll(2);
+        game.roll(7);
+        game.roll(10);
+        assertThat(game.getFrame(2).score()).isEqualTo(null);
+    }
+
+    @Test
+    void should_calculateScoreOfSecondFrame_when_itsTimeForStrike2() {
+        Game game = createNewGame();
+        game.roll(2);
+        game.roll(7);
+        game.roll(10);
+        game.roll(3);
+        assertThat(game.getFrame(2).score()).isEqualTo(null);
+    }
+
+    @Test
+    void should_calculateScoreOfSecondFrame_when_itsTimeForStrike3() {
+        Game game = createNewGame();
+        game.roll(2);
+        game.roll(7);
+        game.roll(10);
+        game.roll(3);
+        game.roll(5);
+        assertThat(game.getFrame(2).score()).isEqualTo(18);
+    }
+
     private Game createNewGame() {
         return new Game();
     }
