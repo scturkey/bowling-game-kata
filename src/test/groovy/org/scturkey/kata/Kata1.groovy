a70c02b7b633def6adeab4f97422010da937d7a6
Scotty <sdlc@scotty.com.tr>
2019-01-10 19:37:17 +0300

    lemi-1

--- src/test/groovy/org/scturkey/kata/Kata1.groovy
+++ src/test/groovy/org/scturkey/kata/Kata1.groovy
@@ -4,6 +4,17 @@ import spock.lang.Specification
 
 class Kata1 extends Specification {
 
+    def "should not find any transaction when no transaction passed"() {
+        given:
+        def transactions = []
+
+        when:
+        def rejected = findRejectedTransactions(transactions, 0)
+
+
+        then:
+        rejected.size() == 0
+    }
 
 
 }
