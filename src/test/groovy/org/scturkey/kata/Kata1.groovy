4bb6d5c13378af63eb491d92a46e89d314e6294b
Scotty <sdlc@scotty.com.tr>
2019-01-10 19:51:54 +0300

    lemi-2

--- src/test/groovy/org/scturkey/kata/Kata1.groovy
+++ src/test/groovy/org/scturkey/kata/Kata1.groovy
@@ -24,17 +24,20 @@ class Kata1 extends Specification {
         size == findRejectedTransactions(transactionList, creditLimit).size()
 
         where:
-        transactionList                      | creditLimit | size
-        []                                   | 100         | 0
-        ["John,Doe,john@doe.com,100,TR0001"] | 100         | 0
-        ["John,Doe,john@doe.com,101,TR0001"] | 100         | 1
+        transactionList                                                        | creditLimit | size
+        []                                                                     | 100         | 0
+        ["John,Doe,john@doe.com,100,TR0001"]                                   | 100         | 0
+        ["John,Doe,john@doe.com,101,TR0001"]                                   | 100         | 1
+        ["John,Doe,john@doe.com,100,TR0001", "John,Doe,john@doe.com,1,TR0001"] | 100         | 1
 
     }
 
     List findRejectedTransactions(List<String> transactionList, int creditLimit) {
+        def sum = 0
         return transactionList.findAll { it ->
             def transactionAmount = valueOf(it.split(",")[3])
-            creditLimit < transactionAmount
+            sum += transactionAmount
+            creditLimit < sum
         }
     }
 }
